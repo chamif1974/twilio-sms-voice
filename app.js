06e6f59ebbe8b316c91c8c6a33cdcb76ce2a9958
Milroy Fernando <mfernand@lk.ibm.com>
2017-03-04 11:00:15 -0600

    cc

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -20,6 +21,80 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+
+var contexts = [];
+
+app.get('/smssent', function (req, res) {
+  var message = req.query.Body;
+  var number = req.query.From;
+  var twilioNumber = req.query.To;
+
+  var context = null;
+  var index = 0;
+  var contextIndex = 0;
+  contexts.forEach(function(value) {
+    console.log(value.from);
+    if (value.from == number) {
+      context = value.context;
+      contextIndex = index;
+    }
+    index = index + 1;
+  });
+
+  console.log('Recieved message from ' + number + ' saying \'' + message  + '\'');
+
+  var conversation = new ConversationV1({
+    username: '',
+    password: '',
+    version_date: ConversationV1.VERSION_DATE_2016_09_20
+  });
+
+  console.log(JSON.stringify(context));
+  console.log(contexts.length);
+
+  conversation.message({
+    input: { text: message },
+    workspace_id: '',
+    context: context
+   }, function(err, response) {
+       if (err) {
+         console.error(err);
+       } else {
+         console.log(response.output.text[0]);
+         if (context == null) {
+           contexts.push({'from': number, 'context': response.context});
+         } else {
+           contexts[contextIndex].context = response.context;
+         }
+
+         var intent = response.intents[0].intent;
+         console.log(intent);
+         if (intent == "done") {
+           //contexts.splice(contexts.indexOf({'from': number, 'context': response.context}),1);
+           contexts.splice(contextIndex,1);
+           // Call REST API here (order pizza, etc.)
+         }
+
+         var client = require('twilio')(
+           '',
+           ''
+         );
+
+         client.messages.create({
+           from: twilioNumber,
+           to: number,
+           body: response.output.text[0]
+         }, function(err, message) {
+           if(err) {
+             console.error(err.message);
+           }
+         });
+       }
+  });
+
+  res.send('');
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {

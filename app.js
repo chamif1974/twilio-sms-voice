fe77d2746d637ede18f41748ce18e103c76385a9
Milroy Fernando <mfernand@lk.ibm.com>
2017-03-05 22:00:28 -0600

    cc

--- app.js
+++ app.js
@@ -25,75 +25,76 @@ var appEnv = cfenv.getAppEnv();
 
 var contexts = [];
 
-app.get('/smssent', function (req, res) {
-  var message = req.query.Body;
-  var number = req.query.From;
-  var twilioNumber = req.query.To;
+app.get('/smssent',function(req,res){
+	var message = req.query.Body;
+	var number = req.query.From;
+	var twilionumber = req.query.To;
+	
+	//var message = 'make appoinment';
+	//var number = '+94776186353';
+	//var twilionumber = '+1 512-865-4719';
+	
+	var context = null;
+	var index = 0;
+	var contextIndex = 0;
+	contexts.forEach(function(value){
+		console.log(value.from);
+		if (value.from === number){
+			context = value.context;
+			contextIndex = index;
+		}
+		index = index + 1;
+	});
+	
+	console.log('Received message from '+ number + 'saying\''+ message +'\'');
+	
+	var conversation = new ConversationV1({
+		username:'130414c3-fb08-4178-a3c3-9cf787b088c8',
+		password:'NtQwAVkKZJ6M',
+		version_date:'2016-10-21'
+	});
+	console.log(JSON.stringify(context));
+	//console.log(context.length);
+	
+	conversation.message({
+		input:{text:message},
+		workspace_id:'0c351ca9-1092-4078-823f-69726c911589',
+		context:context
+	},function(err,response){
+		if(err){
+			console.error(err);
+		} else {
+			console.log(response.output.text[0]);
+			if(context===null){
+				contexts.push({'from':number,'context':response.context});
+			} else {
+				contexts[contextIndex].context=response.context;
+			}
+			
+			var intent = response.intents[0].intent;
+			console.log(intent);
+			if(intent==="done"){
+				contexts.splice(contextIndex,1);
+			}
+			
+			var client = require('twilio')(
+			'ACcb05d8968a6f4d6a3bfd56811d696c19',
+			'6af780368fe871e2dab4955571a5256b'
+			);
+			
+			client.messages.create({
+				from:twilionumber,
+				to:number,
+				body:response.output.text[0]
+			},function(err,message){
+				if(err){
+				console.error(err.message);
+			}
 
-  var context = null;
-  var index = 0;
-  var contextIndex = 0;
-  contexts.forEach(function(value) {
-    console.log(value.from);
-    if (value.from == number) {
-      context = value.context;
-      contextIndex = index;
-    }
-    index = index + 1;
-  });
-
-  console.log('Recieved message from ' + number + ' saying \'' + message  + '\'');
-
-  var conversation = new ConversationV1({
-    username: '',
-    password: '',
-    version_date: ConversationV1.VERSION_DATE_2016_09_20
-  });
-
-  console.log(JSON.stringify(context));
-  console.log(contexts.length);
-
-  conversation.message({
-    input: { text: message },
-    workspace_id: '',
-    context: context
-   }, function(err, response) {
-       if (err) {
-         console.error(err);
-       } else {
-         console.log(response.output.text[0]);
-         if (context == null) {
-           contexts.push({'from': number, 'context': response.context});
-         } else {
-           contexts[contextIndex].context = response.context;
-         }
-
-         var intent = response.intents[0].intent;
-         console.log(intent);
-         if (intent == "done") {
-           //contexts.splice(contexts.indexOf({'from': number, 'context': response.context}),1);
-           contexts.splice(contextIndex,1);
-           // Call REST API here (order pizza, etc.)
-         }
-
-         var client = require('twilio')(
-           '',
-           ''
-         );
-
-         client.messages.create({
-           from: twilioNumber,
-           to: number,
-           body: response.output.text[0]
-         }, function(err, message) {
-           if(err) {
-             console.error(err.message);
-           }
-         });
-       }
-  });
-
-  res.send('');
+			});
+		}
+	});
+	res.send('');
 });
 
 // start server on the specified port and binding host
